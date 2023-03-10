efab6a4fff6dc12bac2d66a767a83cb185f1cb9a
hickr <rickrrneves@gmail.com>
2023-03-10 03:47:30 -0300

    Add project files.

---
+++ FI.AtividadeEntrevista/BLL/BoCliente.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoCliente
+    {
+        /// <summary>
+        /// Inclui um novo cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public long Incluir(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+
+            return cli.Incluir(cliente);
+            
+        }
+
+        /// <summary>
+        /// Altera um cliente
+        /// </summary>
+        /// <param name="cliente">Objeto de cliente</param>
+        public void Alterar(DML.Cliente cliente)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Alterar(cliente);
+        }
+
+        /// <summary>
+        /// Consulta o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public DML.Cliente Consultar(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Consultar(id);
+        }
+
+        /// <summary>
+        /// Excluir o cliente pelo id
+        /// </summary>
+        /// <param name="id">id do cliente</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            cli.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Listar()
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Listar();
+        }
+
+        /// <summary>
+        /// Lista os clientes
+        /// </summary>
+        public List<DML.Cliente> Pesquisa(int iniciarEm, int quantidade, string campoOrdenacao, bool crescente, out int qtd)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.Pesquisa(iniciarEm,  quantidade, campoOrdenacao, crescente, out qtd);
+        }
+
+        /// <summary>
+        /// Verifica a Existencia de um CPF no banco
+        /// </summary> 
+        /// <param name="CPF"></param>
+        /// <returns></returns>
+        public bool VerificarExistencia(string CPF)
+        {
+            DAL.DaoCliente cli = new DAL.DaoCliente();
+            return cli.VerificarExistencia(CPF);
+        }
+
+
+        /// <summary>
+        /// Verifica se o CPF é válido
+        /// </summary> 
+        /// <param name="cpf"></param>
+        /// <returns></returns>
+        public static bool ValidaCPF(string cpf, bool vazio)
+        {
+            if (cpf.Length == 3)
+                return vazio;
+            
+            if (!string.IsNullOrEmpty(cpf) && cpf != "______________")
+            {
+                int[] d = new int[14];
+                int[] v = new int[2];
+                int j, i, soma;
+                string soNumero;
+
+                soNumero = Regex.Replace(cpf, "[^0-9]", string.Empty);
+
+                //Verificando se todos os números são iguais
+                if (new string(soNumero[0], soNumero.Length) == soNumero) return false;
+
+                if (soNumero.Length == 11)
+                {
+                    for (i = 0; i <= 10; i++) d[i] = Convert.ToInt32(soNumero.Substring(i, 1));
+
+                    for (i = 0; i <= 1; i++)
+                    {
+                        soma = 0;
+                        for (j = 0; j <= 8 + i; j++) soma += d[j] * (10 + i - j);
+                        v[i] = (soma * 10) % 11;
+                        if (v[i] == 10) v[i] = 0;
+                    }
+                    return (v[0] == d[9] & v[1] == d[10]);
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                return vazio;
+            }
+        }
+    }
+}
